49308fe45904cce00fd302adb87f8dff9db734af
NikLuc95 <nicola.lucchese@outlook.com>
2025-03-03 02:10:38 +0200

    main.py updated - counting words

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-def get_book_text(path_to_file):
+#def get_book_text(path_to_file):
+#
+#    with open(path_to_file) as f:
+#        file_contents = f.read()
+#   return file_contents
+#
+#file_contents = get_book_text("/home/nik/workspace/github.com/NikLuc95/bookbot/books/frankenstein.txt")
+#
+#def main():
+#
+#    print(file_contents)
+
+def count_book_text(path_to_file):
 
     with open(path_to_file) as f:
         file_contents = f.read()
-    return file_contents
+        num_words = len(file_contents.split())
 
-file_contents = get_book_text("/home/nik/workspace/github.com/NikLuc95/bookbot/books/frankenstein.txt")
+    return num_words
+
+num_words = count_book_text("/home/nik/workspace/github.com/NikLuc95/bookbot/books/frankenstein.txt")
 
 def main():
 
-    print(file_contents)
-    
+    print(f"{num_words} words found in the document")
+
 main()
